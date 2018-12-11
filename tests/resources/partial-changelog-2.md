5f05e8181982e8e27470051d1122e504e598c75e
Erwan Richard <erwan.richard@lephare.com>
2018-12-11 15:06:50 +0100

    Add Merge command

---
+++ tests/resources/partial-changelog-2.md
@@ -0,0 +1,7 @@
+# Change log
+This is a change test change log
+
+## [Unreleased][unreleased]
+### Fixed
+
+- Unreleased fixed issue 2
---
+++ tests/unit/Console/MergeTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * PHP Version 5.6
+ *
+ * @category Library
+ *
+ * @author Steve "uru" West <steven.david.west@gmail.com>
+ * @license MIT http://opensource.org/licenses/MIT
+ *
+ * @see https://github.com/stevewest/changelog
+ */
+
+namespace ChangeLog\Console;
+
+use Codeception\TestCase\Test;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class MergeTest extends Test
+{
+	/**
+	 * @var CommandTester
+	 */
+	protected $commandTester;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$application = new Application();
+		$application->add(new Merge('merge'));
+
+		$command = $application->find('merge');
+		$this->commandTester = new CommandTester($command);
+	}
+
+	public function testMergeMultipleChangelog()
+	{
+		$outputPath = __DIR__.'/../../_output/changelog.json';
+		@unlink($outputPath);
+
+		$this->commandTester->execute([
+			'files' => [
+				__DIR__.'/../../resources/partial-changelog-1.md',
+				__DIR__.'/../../resources/partial-changelog-2.md',
+			],
+			'--config' => __DIR__.'/../../resources/changelog.config.merge.php',
+		]);
+
+		$this->assertFileExists($outputPath);
+		$jsonContent = json_decode(file_get_contents($outputPath), true);
+
+		$this->assertCount(2, $jsonContent['releases']['unreleased']['changes']['Fixed']);
+	}
+}
